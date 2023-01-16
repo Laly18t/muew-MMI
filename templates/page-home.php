d4c8eb97312847632f106e577df67b3bfe6fff3e
Lalou <laly18t@users.noreply.github.com>
2023-01-16 14:58:37 +0100

    integration illustration

--- templates/page-home.php
+++ templates/page-home.php
@@ -9,13 +9,30 @@ get_header();
 
 // echo( $post->post_content);
 
+$introduction= get_field('introduction');
+$image = get_field('image');
 ?>
 
-<h1><?php echo (get_the_title()); ?></h1> 
-
+<article class="landing">
+    <h1><?php echo($introduction['title']); ?></h1>
+    <p class="h1-def"><?php echo($introduction['subtitle']); ?></p>
+    <p><?php echo($introduction['list_title']); ?></p>
+    <ul>
+        <?php foreach($introduction['list'] as $line): ?>
+            <li><?php echo($line['line']); ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <?php if(!empty(get_field($image))) : ?>
+        <img 
+    src="<?php echo($image["sizes"]["medium_large"]); ?>" 
+    height="<?php echo($image["sizes"]["medium_large-height"])?>" 
+    width="<?php echo($image["sizes"]["medium_large-width"])?>"
+    alt="<?php echo($image["alt"])?>" >
+<?php endif; ?>
+</article>
 <?php
 
-//------------------------------- FOOTER ---------------------------
+//--------------------------- FOOTER ---------------------------
 get_footer();
 
 ?>
