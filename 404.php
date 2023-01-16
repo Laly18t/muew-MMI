dd512a5d026d30072bfdd9b7f8a3e90286068372
Lalou <laly18t@users.noreply.github.com>
2023-01-16 18:11:41 +0100

    Custom 404

--- 404.php
+++ 404.php
@@ -3,11 +3,11 @@
 //------------------------------- HEADER ---------------------------
 get_header();
 
+$not_found= get_field('not_found');
 ?>
 <div class="404">
-    <p>Erreur 404</p>
-    <p>Page introuvable</p>
-    <p class="txt-info">Désolé ! Cette page n'est pas disponible 🛠️</p>
+    <img src="" alt="">
+    <p><?php echo($not_found['not_found']); ?></p>
 </div>
 
 </body>
