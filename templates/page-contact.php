b5a0e5f9d535e1c39f5995b5c97d2190adedf562
Lalou <laly18t@users.noreply.github.com>
2023-01-19 13:47:27 +0100

    modif contact

--- templates/page-contact.php
+++ templates/page-contact.php
@@ -21,46 +21,41 @@ $form= get_field('form');
 
     <!-- section formulaire de contacte -->
     <section>
+        <form method="post" action="">
         <div class="grid-form">
-            <form method="post" action=""></form>
-            <input type="hidden" name="contact">
-            <div>
-                <label for="first_name"><?php echo($form['first_name']); ?>:</label>
-                <input type="text" name="first_name" placeholder="<?php echo($form['first_name']); ?>">
+                <div>
+                    <label for="first_name"><?php echo($form['first_name']); ?>:</label>
+                    <input type="text" name="first_name" placeholder="<?php echo($form['first_name']); ?>">
+                </div>
+                <div>
+                    <label for="name"><?php echo($form['name']); ?>:</label>
+                    <input type="text" name="name" placeholder="<?php echo($form['name']); ?>">
+                </div>
+                <div>
+                    <label for="email"><?php echo($form['email']); ?>:</label>
+                    <input type="text" name="email" placeholder="<?php echo($form['email']); ?>">
+                </div>
+                <div>
+                    <label for="phone"><?php echo($form['phone']); ?>:</label>
+                    <input type="text" name="phone" placeholder="<?php echo($form['phone']); ?>">
+                </div>
+                <div>
+                    <label for="society"><?php echo($form['society']); ?>:</label>
+                    <input type="text" name="society" placeholder="<?php echo($form['society']); ?>">
+                </div>
+                <div>
+                    <label for="function"><?php echo($form['function']); ?></label>
+                    <input type="text" name="function" placeholder="<?php echo($form['function']); ?>">
+                </div>
             </div>
-            <div>
-                <label for="name"><?php echo($form['name']); ?>:</label>
-                <input type="text" name="name" placeholder="<?php echo($form['name']); ?>">
+            <div class="form-desc">
+                <label for="message"><?php echo($form['message']); ?></label>
+                <input type="text" name="message" placeholder="<?php echo($form['message']); ?>">
+                
+                <h5><?php echo($form['confidentiality']); ?></h5>
+                <button class="button"><?php echo($form['send']); ?></button>
             </div>
-            <div>
-                <p><?php echo($form['email']); ?></p>
-                <label for="email"><?php echo($form['email']); ?>:</label>
-                <input type="text" name="email" placeholder="<?php echo($form['email']); ?>">
-            </div>
-            <div>
-                <p><?php echo($form['phone']); ?></p>
-                <label for="phone"><?php echo($form['phone']); ?>:</label>
-                <input type="text" name="phone" placeholder="<?php echo($form['phone']); ?>">
-            </div>
-            <div>
-                <p><?php echo($form['society']); ?></p>
-                <label for="society"><?php echo($form['society']); ?>:</label>
-                <input type="text" name="society" placeholder="<?php echo($form['society']); ?>">
-            </div>
-            <div>
-                <p><?php echo($form['function']); ?></p>
-                <label for="function"><?php echo($form['function']); ?>:</label>
-                <input type="text" name="function" placeholder="<?php echo($form['function']); ?>">
-            </div>
-        </div>
-        <div class="form-desc">
-            <p><?php echo($form['message']); ?></p>
-            <label for="message"><?php echo($form['message']); ?>:</label>
-            <input type="text" name="message" placeholder="<?php echo($form['message']); ?>">
-
-            <h5><?php echo($form['confidentiality']); ?></h5>
-            <button class="button"><?php echo($form['send']); ?></button>
-        </div>
+        </form>
     </section>
 
 <?php
