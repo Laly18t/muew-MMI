4e09c35346d1d97814fcce1532433b9ef201d675
Lalou <laly18t@users.noreply.github.com>
2023-01-20 13:57:26 +0100

    update

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
 
-wp_enqueue_style('app', get_template_directory_uri().'/css/app.css', array(), 1, 'all');
+function add_style() {
+  wp_enqueue_style('app', get_template_directory_uri().'/css/app.css', array(), 1, 'all');
+}
+add_action( 'wp_enqueue_scripts', 'add_style' );
 
 
 add_theme_support("menus");
