bf24fa2d42e2e0dbca4699746883d945c43b9225
Lalou <laly18t@users.noreply.github.com>
2023-02-08 20:34:05 +0100

    debug

--- templates/mention.php
+++ templates/mention.php
@@ -7,13 +7,13 @@
 //------------------------------- HEADER ---------------------------
 get_header();
 
-// $mention= get_field('mention');
-// var_dump($mention);
+$mention= get_field('mention');
+var_dump($mention);
 ?>
 
 
 <div class="s2_ment">
-    <?php foreach(get_field('mention'['paragraph']) as $paragraph): ?>
+    <?php foreach($mention['paragraph'] as $paragraph): ?>
         <h2><?php echo($paragraph['title_paragraph']); ?></h2>
         <p><?php echo($paragraph['text_paragraph']); ?></p>
     <?php endforeach; ?>
